5b76ee429fff8fa644b6ef9398e508294a97604e
yesolll <9595kys@gmail.com>
2023-02-21 14:36:51 +0900

    feat: 초기 보안 설정 - SecurityFilterChain - PasswordEncoder

---
+++ back/src/main/java/com/pre012/server/config/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.pre012.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfiguration {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable() // stateless(jwt)
+            .cors(Customizer.withDefaults())
+            .formLogin().disable()
+            .httpBasic().disable()
+            .authorizeHttpRequests(authorize -> authorize
+                                    .anyRequest().permitAll() // 모든 요청 허용 -> 변경 필요
+            )
+            ;
+        return http.build(); 
+    }
+
+    @Bean
+    public PasswordEncoder createPasswordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+        
+}

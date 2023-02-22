7ecb32c667f51d69a772f880d925672fe99bc586
yesolll <9595kys@gmail.com>
2023-02-22 19:01:57 +0900

    feat: [Member] 로그인 API 구현

--- back/src/main/java/com/pre012/server/member/controller/MemberController.java
+++ back/src/main/java/com/pre012/server/member/controller/MemberController.java
@@ -2,13 +2,11 @@ package com.pre012.server.member.controller;
 
 import javax.validation.Valid;
 
+import com.pre012.server.member.entity.Member;
+import com.pre012.server.member.mapper.MemberMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.pre012.server.member.dto.MemberDto.SignUpDto;
 import com.pre012.server.member.service.MemberService;
@@ -19,14 +17,18 @@ import com.pre012.server.member.service.MemberService;
 public class MemberController {
     
     private final MemberService memberService;
+    private final MemberMapper memberMapper;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, MemberMapper memberMapper) {
         this.memberService = memberService;
+        this.memberMapper = memberMapper;
     }
 
     @PostMapping
-    public ResponseEntity signup(@Valid SignUpDto signUpDto) {
-        return null;
+    public ResponseEntity<HttpStatus> signUp(@Valid @RequestBody SignUpDto signUpDto) {
+        Member member = memberMapper.signUpDtoToMember(signUpDto);
+        memberService.createMember(member);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/profile")
--- back/src/main/java/com/pre012/server/member/service/MemberService.java
+++ back/src/main/java/com/pre012/server/member/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.pre012.server.member.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.pre012.server.auth.util.CustomAuthorityUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.pre012.server.member.entity.Member;
@@ -11,24 +14,32 @@ import com.pre012.server.member.repository.MemberRepository;
 public class MemberService {
     
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final CustomAuthorityUtils authorityUtils;
 
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository,
+                         PasswordEncoder passwordEncoder,
+                         CustomAuthorityUtils authorityUtils) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authorityUtils = authorityUtils;
     }
 
     public void createMember(Member member) {
-
+        verifyAlreadyExistsEmailOrDisplayName(member.getEmail(), member.getDisplayName());
+        encryptPassword(member);
+        assignRole(member);
+        memberRepository.save(member);
     }
 
     public void verifyMember(Long memberId) {
         Optional<Member> member = memberRepository.findById(memberId);
-        if (!member.isPresent()) throw new RuntimeException(); // 예외처리 나중에 바꾸겠습니다 (23.02.22)
+        if (member.isEmpty()) throw new RuntimeException(); // 예외처리 나중에 바꾸겠습니다 (23.02.22)
     }
 
     public Member findVerifiedMember(Long memberId) {
         Optional<Member> member = memberRepository.findById(memberId);
-        Member findMember = member.orElseThrow(() -> new RuntimeException());
-        return findMember;
+        return member.orElseThrow(RuntimeException::new);
     }
 
     public void verifyAlreadyExistsEmailOrDisplayName(String email, String displayName) {
@@ -40,9 +51,14 @@ public class MemberService {
             throw new RuntimeException();
     }
 
-    // private void encryptPassword(Member member) {
-    //     String encryptedPassword = passwordEncoder.encode(member.getPassword());
-    //     member.setPassword(encryptedPassword);
-    // }
+     private void encryptPassword(Member member) {
+         String encryptedPassword = passwordEncoder.encode(member.getPassword());
+         member.setPassword(encryptedPassword);
+     }
+
+    private void assignRole(Member member) {
+        List<String> roles = authorityUtils.createRoles(member.getEmail());
+        member.setRoles(roles);
+    }
 
 }

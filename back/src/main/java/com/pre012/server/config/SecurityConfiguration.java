0c75eadf8f703b193cd81bbc7c0a0c44f0a52e6d
yesolll <9595kys@gmail.com>
2023-02-22 19:34:46 +0900

    docs: Security 로깅 주석처리

--- back/src/main/java/com/pre012/server/config/SecurityConfiguration.java
+++ back/src/main/java/com/pre012/server/config/SecurityConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+//@EnableWebSecurity(debug = true)
 public class SecurityConfiguration {
 
     @Bean

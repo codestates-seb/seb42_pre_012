92b9e999b5a7084794eeb4822e99c880c455eaa8
yesolll <9595kys@gmail.com>
2023-02-27 22:34:35 +0900

    feat: Member 유효성

--- back/src/main/java/com/pre012/server/member/controller/MemberController.java
+++ back/src/main/java/com/pre012/server/member/controller/MemberController.java
@@ -19,10 +19,12 @@ import com.pre012.server.question.entity.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/members")
+@Validated
 public class MemberController {
     
     private final MemberService memberService;
